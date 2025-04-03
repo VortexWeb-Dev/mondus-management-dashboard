a20810ebb014f20efb302cae5c01cbda04b56de4
fasilofficial <muhammedfasilofficial@gmail.com>
2025-04-03 10:05:23 +0530

    Update crest/settings

--- crest/settings.php
+++ crest/settings.php
@@ -1,6 +1,5 @@
 <?php
-define('C_REST_WEB_HOOK_URL', 'https://mondus.group/rest/1/c3iwswykiqvhzc71/'); //url on creat Webhook
-// define('C_REST_CLIENT_ID', 'local.6764fde435a768.91724346');
-// define('C_REST_CLIENT_SECRET', 'M8Dl22t5VJJMwE1ZEEmdRy5q5JyttoPhUn4BgfVZ49g55kUtji');
+define('C_REST_WEB_HOOK_URL', 'https://mondus.group/rest/1/dw9gd4xauhctd7ha/'); //url on creat Webhook
+define('C_REST_CLIENT_ID', 'local.67eccaf5dc9d08.62533272');
+define('C_REST_CLIENT_SECRET', 'otW7cBNoEfI7Wx3amN9GFeBRIIpe8Olqxe033LuX7Eb2qfh2Uq');
 define('C_REST_BLOCK_LOG', true); //Application ID
- 
